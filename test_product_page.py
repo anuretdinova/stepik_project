e4c74b32027500131006aabd08540ddfb903982d
anuretdinova <66945893+anuretdinova@users.noreply.github.com>
2020-06-23 17:26:24 +0400

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,9 @@
 import pytest
 import time
 import random
-from pages.basket_page import BasketPage
-from pages.login_page import LoginPage
-from pages.product_page import ProductPage
+from pages.basket_page import BasketPage  # если не запускается добавить точку перед "pages.basket_page import BasketPage"  (разница пакетов)
+from pages.login_page import LoginPage  # если не запускается добавить точку перед "pages.login_page import LoginPage"  (разница пакетов)
+from pages.product_page import ProductPage  # если не запускается добавить точку перед "pages.product_page import ProductPage"  (разница пакетов)
 
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
